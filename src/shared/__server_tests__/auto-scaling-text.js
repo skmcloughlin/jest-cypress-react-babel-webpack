9abef3e4c1e6d4925d907603d83257ba6e7ffbfa
Shaun McLoughlin <skmcloughlin@gmail.com>
2020-05-20 10:39:10 -0400

    finished 20

--- src/shared/__server_tests__/auto-scaling-text.js
+++ src/shared/__server_tests__/auto-scaling-text.js
@@ -1,6 +1,4 @@
-import React from 'react'
 import ReactDomServer from 'react-dom/server'
-import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
   ReactDomServer.renderToString()
